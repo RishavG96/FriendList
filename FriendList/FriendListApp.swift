c86fcc46d964a7f3a58468392b76fe137d16c95f
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-22 05:39:10 +0530

    Initial Commit

---
+++ FriendList/FriendListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendListApp.swift
+//  FriendList
+//
+//  Created by Rishav Gupta on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
